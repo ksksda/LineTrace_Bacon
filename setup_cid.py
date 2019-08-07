85410f951678e1c00e45442374f3cf2fc4207202
RYUSEI <s17c1024gb@s.chibakoudai.jp>
2019-08-08 06:35:23 +0900

    chg servo

--- setup_cid.py
+++ setup_cid.py
@@ -4,9 +4,17 @@ import rsc_u485
 if __name__ == '__main__':
     servo = rsc_u485.RSC_U485('/dev/ttyUSB0',460800)
 
-    print("cid?")
-    cid=input()
+
+    cid=int(input('cid?'))
     servo.IDchange(int(cid))
+    servo.IDchange(cid)
     time.sleep(1)
     servo.RomSave()
     time.sleep(1)
+    servo.torque(cid,1)
+    servo.move(cid,1000,200)
+    time.sleep(2)
+    servo.move(cid,-1000,200)
+    time.sleep(2)
+    servo.move(cid,0,200)
+    time.sleep(2)
