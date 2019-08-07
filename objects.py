7a57dd21a57d7299fe240cca3f3427d66fee7112
ksksda <33217713+ksksda@users.noreply.github.com>
2019-08-07 16:55:02 +0900

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,21 +1,35 @@
-def motor_control(mL, mR):
-	while True:
-		yield (dL + dR)/2.
+import serial
+import time
 
-def servo_control(angles):
+class robot_control(object):
+	def __init__(self):
+		self.gpio_seri = serial.Serial('/dev/ttyACM0',9600)
+		self.motor = [0., 0.]
+		self.servo = [0, 0, 0, 0, 60]
+		self.gate = [0, 0, 0]
+		self.sensor = [0, 0, 0]
+		self.dist = 0.
+		
+	def __iter__(self):
+		return self
+	def __next__(self):
+		snd = ''
+		for f in self.motor:
+			snd += str(f) + ' '
+		for f in self.servo:
+			snd += str(f) + ' '
+		for f in self.gate:
+			snd += str(f) + ' '
+		self.gpio_seri.write(snd + 'e')
+		ret = ''
+		while not ret:
+			time.sleep(0.005)
+			ret = self.gpio_seri.readline()
+		r = ret.split(' ')
+		for i in range(3):
+			self.sensor[i] = int(r[i])
+		self.dist = float(r[3])
 	
-
-def gate_control(i):
-	
-
-def sense_line():
-	return sense
-
-def identify_color():
-	return clr
-
-def measure_dist():
-	return dist
-
-mc = motor_control(0, 0)
-
+	def identify_color(self):
+		clr = 0
+		return clr
