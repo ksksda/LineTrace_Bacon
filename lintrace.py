512a4219b0ee4504fde834fbb06d7f6908e40e59
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2019-08-07 18:40:42 +0900

    add linetrace and fix indent

--- lintrace.py
+++ lintrace.py
@@ -1,7 +1,87 @@
 import capture
+import time
 
-def straight():		# 最初にラインを見つけるまで走る
-def linetrace():	# ライントレース、ラインクロス1回でcapture.throw、4回でcapture.seek
-def shoot():		# ボールを全部拾ったあと、ボールが落ちているフィールドにおいてその場転回した後に実行される。ボールをゴールに入れる
-def goal():			# 初期位置に戻って止まる
+line_crossed = 0
+off_flag = False
 
+def straight(rc):		# 最初にラインを見つけるまで走る
+    rc.motor = [0.5,0.5]
+    yield None
+    while True:
+        if any(rc.sensor):
+            yield linetrace
+        yield None
+
+def linetrace(rc):	# ライントレース、ラインクロス1回でcapture.throw、4回でcapture.seek
+    yield None
+    while True:
+        if all(rc.sensor):
+            if not off_flag:
+                line_crossed += 1
+                off_flag = True
+                if line_crossed == 2:
+                    yield capture.throw
+                elif line_crossed == 4:
+                    yield capture.seek
+                elif line_crossed == 6:
+                    yield capture.shoot
+                # elif line_crossed >= 7:
+                #     yield capture.shoot2
+        else:
+            off_flag = False
+        
+        if not off_flag:
+            if rc.sensor[0]:
+                rc.motor = [0.25,0.75]
+            elif rc.sensor[2]
+                rc.motor = [0.75,0.25]
+            else:
+                rc.motor = [0.5,0.5]
+        yield None
+
+def shoot(rc):		# ボールを全部拾ったあと、ボールが落ちているフィールドにおいてその場転回した後に実行される。ボールをゴールに入れる
+    rc.motor = [0.5,0.5]
+    yield None
+    while True:
+        if not all(rc.sensor):
+            if off_flag:
+                off_flag = False
+        if all(rc.sensor) and not off_flag:
+            break
+
+    rc.motor = [0.5,-0.5]
+    yield None
+    while True:
+        if not any(rc.sensor):
+            break
+        yield None
+    while True:
+        if rc.sensor[1]:
+            break
+        yield None
+    rc.motor = [0,0]
+    rc.gate = [1,1,1]
+    yield None
+    time.sleep(1.0)
+    for i in range(3):
+        rc.gate[1] = 0
+        yield None
+        time.sleep(0.5)
+    # rc.motor = [0.5,-0.5]
+    # yield None
+    # while True:
+    #     if not any(rc.sensor):
+    #         break
+    #     yield None
+    # while True:
+    #     if all(rc.sensor):
+    #         yield linetrace
+    #     yield None
+    
+
+def shoot2(rc):		# ボールを全部拾ったあと、ボールが落ちているフィールドにおいてその場転回した後に実行される。ボールをゴールに入れる
+    #TODO 将来の課題
+    pass
+
+def goal(rc):			# 初期位置に戻って止まる
+    pass
--- objects.py
+++ objects.py
@@ -2,34 +2,34 @@ import serial
 import time
 
 class robot_control(object):
-	def __init__(self):
-		self.gpio_seri = serial.Serial('/dev/ttyACM0',9600)
-		self.motor = [0., 0.]
-		self.servo = [0, 0, 0, 0, 60]
-		self.gate = [0, 0, 0]
-		self.sensor = [0, 0, 0]
-		self.dist = 0.
-		
-	def __iter__(self):
-		return self
-	def __next__(self):
-		snd = ''
-		for f in self.motor:
-			snd += str(f) + ' '
-		for f in self.servo:
-			snd += str(f) + ' '
-		for f in self.gate:
-			snd += str(f) + ' '
-		self.gpio_seri.write(snd + 'e')
-		ret = ''
-		while not ret:
-			time.sleep(0.005)
-			ret = self.gpio_seri.readline()
-		r = ret.split(' ')
-		for i in range(3):
-			self.sensor[i] = int(r[i])
-		self.dist = float(r[3])
-	
-	def identify_color(self):
-		clr = 0
-		return clr
+    def __init__(self):
+        self.gpio_seri = serial.Serial('/dev/ttyACM0',9600)
+                self.motor = [0., 0.]
+                self.servo = [0, 0, 0, 0, 60]
+                self.gate = [0, 0, 0]
+                self.sensor = [0, 0, 0]
+                self.dist = 0.
+
+        def __iter__(self):
+            return self
+        def __next__(self):
+            snd = ''
+                for f in self.motor:
+                    snd += str(f) + ' '
+                for f in self.servo:
+                    snd += str(f) + ' '
+                for f in self.gate:
+                    snd += str(f) + ' '
+                self.gpio_seri.write(snd + 'e')
+                ret = ''
+                while not ret:
+                    time.sleep(0.005)
+                        ret = self.gpio_seri.readline()
+                r = ret.split(' ')
+                for i in range(3):
+                    self.sensor[i] = int(r[i])
+                self.dist = float(r[3])
+
+        def identify_color(self):
+            clr = 0
+                return clr
