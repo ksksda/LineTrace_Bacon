707b4ebd76157e13413e49f3db4114efe2e04541
ksksda <33217713+ksksda@users.noreply.github.com>
2019-08-07 16:55:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import linetrace
 import capture
 import objects
 
+rc = objects.robot_control()
 loop_func = linetrace.straight
 
 if __name__ == '__main__':
 	while True:
 		if loop_func:
-			lf = loop_func()
+			lf = loop_func(rc)
 			next(lf)
 			loop_func = None
-		loop_func = lf.send(next(objects.mc))
-
+		loop_func = lf.send(next(rc))
