9d87a374b6df9ec05f17a93f9fd97fc9a82af193
ksksda <s17C1079NG@s.chibakoudai.jp>
2019-12-13 14:57:55 +0900

    ensure the serial port available

--- objects.py
+++ objects.py
@@ -4,38 +4,52 @@ import rsc_u485
 
 class robot_control(object):
     def __init__(self):
-        self.gpio_seri = serial.Serial('/dev/ttyACM0',9600)
-        self.servo = rsc_u485.RSC_U485('/dev/ttyUSB0',460800)
+        
+        for _ in range(10):
+            try:
+                self._arduino = serial.Serial('/dev/ttyACM0',9600)
+                break
+            except FileNotFoundError:
+                self._arduino = None
+        
+        for _ in range(10):
+            try:
+                self._rs485 = rsc_u485.RSC_U485('/dev/ttyUSB0',460800)
+                break
+            except FileNotFoundError:
+                self._rs485 = None
+        
         self.motor = [0., 0.]
         self.servo = [0, 0, 0, 0, 0]
         self.gate = [0, 0, 0]
         self.sensor = [0, 0, 0, 0]
         self.dist = 0.
         self.c = 0
-        for i in range(8):
-            self.servo.torque(i + 1, 1)
+        if self._rs485:
+            for i in range(8):
+                self._rs485.torque(i + 1, 1)
         
     def __iter__(self):
         return self
     def __next__(self):
-        snd = ''
-        for f in self.motor:
-            snd += str(f) + ' '
-        self.gpio_seri.write(snd + 'e')
-            
-        for i in range(5):
-            self.servo.move(i + 1, 10*self.servo[i], 1)
-        for i in range(3):
-            self.servo.move(i + 1, -1200*self.gate[i], 1)
         
-        ret = ''
-        while not ret:
-            time.sleep(0.005)
-            ret = self.gpio_seri.readline()
-        r = ret.split(' ')
-        for i in range(4):
-            self.sensor[i] = int(r[i])
-        self.dist = 34541.0/(float(r[4])+float(r[5])) - 6.0348
+        if self._rs485:
+            for i in range(5):
+                self._rs485.move(i + 1, 10*self.servo[i], 1)
+            for i in range(3):
+                self._rs485.move(i + 1, -1200*self.gate[i], 1)
+        
+        if self._arduino:
+            snd = ''
+            for f in self.motor:
+                snd += str(f) + ' '
+            self._arduino.write(snd + 'e')
+            
+            ret = self._arduino.readline()
+            r = ret.split(' ')
+            for i in range(4):
+                self.sensor[i] = int(r[i])
+            self.dist = 34541.0/(float(r[4])+float(r[5])) - 6.0348
 
     def identify_color(self):
         self.c += 1
