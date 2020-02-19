7bcd8c3b1d7fb3d5bf55fe53873d84776dded8e5
ksksda <s17C1079NG@s.chibakoudai.jp>
2020-02-20 03:25:23 +0900

    display log

--- move_that.py
+++ move_that.py
@@ -3,15 +3,19 @@ import rsc_u485
 
 if __name__ == '__main__':
     servo = rsc_u485.RSC_U485('/dev/ttyUSB0',460800)
+    ang_list = [None,None,None,None,None,None,None,None]
 
     while True:
-    	  cid=int(input('cid?'))
+        cid=int(input('cid?'))
         servo.torque(cid,1)
         print(str(cid) + " servo's torque on!")
         while True:
             print("angle?: ",end="")
             ang=input()
             if ang == 'q':
+                print("--------------------")
                 break
             servo.move(cid,int(ang),100)
+            ang_list[cid-1] = ang
+            print(ang_list)
             time.sleep(1)
